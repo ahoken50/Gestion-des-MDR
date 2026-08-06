b3e7a4fede2cd27f82b3cf93af62ea0aacc4cd99
ahoken50 <micross50@gmail.com>
2026-08-06 16:06:23 -0400

    build: output production build to docs folder on main branch

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
       plugins: [react()],
       base: '/Gestion-des-MDR/',
       build: {
-        outDir: 'dist',
+        outDir: 'docs',
         assetsDir: 'assets',
         sourcemap: true,
       },
